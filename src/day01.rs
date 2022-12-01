7bec87258a3ad3569eb61d7515cf1f0257013e37
vintitres <jaqb@sekta.pl>
2022-12-01 23:56:31 +0000

    01

---
+++ src/day01.rs
@@ -0,0 +1,51 @@
+use std::io::BufRead;
+
+// TODO do I have to Box?
+
+fn elves(input: Box<dyn BufRead>) -> Box<dyn Iterator<Item = i32>> {
+    let mut v = Vec::new();
+    v.push(Vec::new());
+    Box::new(
+        input
+            .lines()
+            .map(|l| l.unwrap())
+            .fold(v, |mut acc, line| {
+                if line.is_empty() {
+                    acc.push(Vec::new());
+                } else {
+                    acc.last_mut().unwrap().push(line.parse::<i32>().unwrap());
+                }
+                acc
+            })
+            // TODO
+            // .split(|l: String| l.is_empty());
+            // .map(|e| e.map(|f| f.parse::<i32>().unwrap()));
+            .into_iter()
+            .map(|x| x.into_iter().sum::<i32>()),
+    )
+}
+
+pub fn a(input: Box<dyn BufRead>) -> i32 {
+    elves(input).max().unwrap()
+}
+
+pub fn b(input: Box<dyn BufRead>) -> i32 {
+    let mut v = Vec::new();
+    v.push(0);
+    v.push(0);
+    v.push(0);
+    elves(input)
+        .fold(v, |mut top3, x| {
+            let mut x = x;
+            for i in 0..3 {
+                if x > top3[i] {
+                    let xx = top3[i];
+                    top3[i] = x;
+                    x = xx;
+                }
+            }
+            top3
+        })
+        .into_iter()
+        .sum()
+}
