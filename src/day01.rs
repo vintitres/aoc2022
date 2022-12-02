e620654429a2d6aa29afaf7f6c7ebced22aade19
vintitres <jaqb@sekta.pl>
2022-12-02 01:47:52 +0000

    01

--- src/day01.rs
+++ src/day01.rs
@@ -6,19 +6,15 @@ fn elves(input: Box<dyn BufRead>) -> Box<dyn Iterator<Item = i32>> {
         input
             .lines()
             .map(|l| l.unwrap())
-            .fold(vec![vec![]], |mut acc, line| {
+            .fold(vec![0], |mut acc, line| {
                 if line.is_empty() {
-                    acc.push(Vec::new());
+                    acc.push(0);
                 } else {
-                    acc.last_mut().unwrap().push(line);
+                    *acc.last_mut().unwrap() += line.parse::<i32>().unwrap();
                 }
                 acc
             })
-            // TODO
-            // .split(|l: String| l.is_empty());
-            .into_iter()
-            .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap()))
-            .map(|x| x.sum::<i32>()),
+            .into_iter(),
     )
 }
 
@@ -28,7 +24,7 @@ pub fn a(input: Box<dyn BufRead>) -> i32 {
 
 pub fn b(input: Box<dyn BufRead>) -> i32 {
     elves(input)
-        .fold(vec![0,0,0], |mut top3, x| {
+        .fold(vec![0, 0, 0], |mut top3, x| {
             let mut x = x;
             for i in 0..3 {
                 if x > top3[i] {
