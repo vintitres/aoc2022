b9e871fd357ff3d76d4718eb0319164c8400ef8b
vintitres <jaqb@sekta.pl>
2022-12-02 05:49:58 +0000

    02b

--- src/day02.rs
+++ src/day02.rs
@@ -43,3 +43,47 @@ pub fn a(input: Box<dyn BufRead>) -> i32 {
             }
         }).sum()
 }
+
+pub fn b(input: Box<dyn BufRead>) -> i32 {
+    input
+        .lines()
+        .map(|l| {
+            let g = l.unwrap();
+            let mut g = g.chars();
+            let op = g.next();
+            let op = op.unwrap();
+            g.next();
+            let me = g.next();
+            let me = me.unwrap();
+            match me {
+                'X' => {
+                    0 +
+                    match op {
+                        'A' => 3,
+                        'B' => 1,
+                        'C' => 2,
+                        c => panic!("{}", c),
+                    }
+                },
+                'Y' => {
+                    3 +
+                    match op {
+                        'A' => 1,
+                        'B' => 2,
+                        'C' => 3,
+                        c => panic!("{}", c),
+                    }
+                },
+                'Z' =>  {
+                    6 +
+                    match op {
+                        'A' => 2,
+                        'B' => 3,
+                        'C' => 1,
+                        c => panic!("{}", c),
+                    }
+                },
+                c => panic!("{}", c),
+            }
+        }).sum()
+}
