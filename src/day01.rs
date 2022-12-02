2d52e6fb7e4083522361fe68d091d1a3ff9ce6f8
vintitres <jaqb@sekta.pl>
2022-12-02 00:01:26 +0000

    01

--- src/day01.rs
+++ src/day01.rs
@@ -1,10 +1,11 @@
 use std::io::BufRead;
 
 // TODO do I have to Box?
-
 fn elves(input: Box<dyn BufRead>) -> Box<dyn Iterator<Item = i32>> {
+    // TODO shorter init?
     let mut v = Vec::new();
     v.push(Vec::new());
+
     Box::new(
         input
             .lines()
@@ -21,7 +22,7 @@ fn elves(input: Box<dyn BufRead>) -> Box<dyn Iterator<Item = i32>> {
             // .split(|l: String| l.is_empty());
             .into_iter()
             .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap()))
-            .map(|x| x.sum::<i32>())
+            .map(|x| x.sum::<i32>()),
     )
 }
 
@@ -30,10 +31,12 @@ pub fn a(input: Box<dyn BufRead>) -> i32 {
 }
 
 pub fn b(input: Box<dyn BufRead>) -> i32 {
+    // TODO shorter init?
     let mut v = Vec::new();
     v.push(0);
     v.push(0);
     v.push(0);
+
     elves(input)
         .fold(v, |mut top3, x| {
             let mut x = x;
