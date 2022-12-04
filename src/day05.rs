89f87a22d549c847710a93aacb708b4c90541346
vintitres <jaqb@sekta.pl>
2022-12-04 23:18:23 +0000

    5template

---
+++ src/day05.rs
@@ -0,0 +1,31 @@
+fn read(input: &str) -> impl Iterator<Item = &str> + '_ {
+    input
+        .lines()
+}
+
+pub fn part1(input: &str) -> usize {
+    read(input).count()
+}
+
+pub fn part2(input: &str) -> usize {
+    read(input).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> &'static str {
+        include_str!("../input/2022/day5.txt")
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(input()), 466);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(input()), 865);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 // pub mod day01;
 // pub mod day02;
 // pub mod day03;
-pub mod day04;
+//pub mod day04;
+pub mod day05;
 
 // use aoc_runner_derive::aoc_lib;
 // aoc_lib! { year = 2022 }
