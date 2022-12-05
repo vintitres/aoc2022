0e848fd9cefca708ae4eb2f495053dc3af6431bd
vintitres <jaqb@sekta.pl>
2022-12-05 08:04:29 +0000

    5

--- src/day05.rs
+++ src/day05.rs
@@ -1,12 +1,9 @@
 use itertools::Itertools;
 
 type Stack = Vec<char>;
-struct Stacks {
-    stacks: Vec<Stack>,
-}
 type Move = (usize, usize, usize);
 
-fn read(input: &str) -> (Stacks, Vec<Move>) {
+fn read(input: &str) -> (Vec<Stack>, Vec<Move>) {
     let mut lines = input.lines();
     let mut stacks = Vec::new();
     for line in lines.by_ref().take_while(|l| !l.starts_with(" 1")) {
@@ -32,36 +29,22 @@ fn read(input: &str) -> (Stacks, Vec<Move>) {
                 .unwrap()
         })
         .collect();
-    (Stacks { stacks }, moves)
+    (stacks, moves)
 }
 
-impl Stacks {
-    pub fn domove(&mut self, (count, from, to): &Move) {
-        // let mut from = self[from - 1];
-        // let mut to = self[to - 1];
-        let to = to - 1;
-        let from = from - 1;
-        let new_len = self.stacks[from].len() - count;
-        let moved = self.stacks[from][new_len..]
-            .iter()
-            .rev()
-            .cloned()
-            .collect_vec();
-        self.stacks.get_mut(to).unwrap().extend(moved);
-        self.stacks[from].resize(new_len, '!');
-    }
+fn domove(stacks: &mut [Stack], (count, from, to): &Move) {
+    let to = to - 1;
+    let from = from - 1;
+    let new_len = stacks[from].len() - count;
+    let moved = stacks[from][new_len..].iter().rev().cloned().collect_vec();
+    stacks.get_mut(to).unwrap().extend(moved);
+    stacks[from].resize(new_len, '!');
 }
 
 pub fn part1(input: &str) -> String {
     let (mut stacks, moves) = read(input);
-    moves.iter().for_each(|m| {
-        stacks.domove(m);
-    });
-    stacks
-        .stacks
-        .iter()
-        .map(|stack| stack.last().unwrap())
-        .collect()
+    moves.iter().for_each(|m| domove(&mut stacks, m));
+    stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
 
 pub fn part2(input: &str) -> String {
@@ -69,16 +52,13 @@ pub fn part2(input: &str) -> String {
     moves.iter().for_each(|(count, from, to)| {
         let to = to - 1;
         let from = from - 1;
-        let new_len = stacks.stacks[from].len() - count;
-        let moved = stacks.stacks[from][new_len..].iter().cloned().collect_vec();
-        stacks.stacks.get_mut(to).unwrap().extend(moved);
-        stacks.stacks[from].resize(new_len, '!');
+        let new_len = stacks[from].len() - count;
+        let moved = stacks[from][new_len..].iter().cloned().collect_vec();
+        stacks.get_mut(to).unwrap().extend(moved);
+        stacks[from].resize(new_len, '!');
+        println!("{:?}", stacks);
     });
-    stacks
-        .stacks
-        .iter()
-        .map(|stack| stack.last().unwrap())
-        .collect()
+    stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
 
 #[cfg(test)]
