209024ef2e624a1fd35029896fcf91e5c5a3ba09
vintitres <vintitres@users.noreply.github.com>
2022-12-05 09:45:38 -1000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-// pub mod day01;
-// pub mod day02;
-// pub mod day03;
-//pub mod day04;
-// pub mod day05;
+#[cfg(alldays)]
+pub mod alldays {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+}
 pub mod day06;
 
 // use aoc_runner_derive::aoc_lib;
