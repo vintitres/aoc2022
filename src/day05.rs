d642e237145162c860e8044575cb9a1a1580ff6a
vintitres <jaqb@sekta.pl>
2022-12-05 07:09:36 +0000

    5

--- src/day05.rs
+++ src/day05.rs
@@ -7,10 +7,7 @@ fn read(input: &str) -> (Vec<Stack>, Vec<Move>) {
     const MAX_STACK: usize = 8;
     const STACK_COUNT: usize = 9;
     let mut lines = input.lines();
-    let mut stacks = Vec::new();
-    for _ in 0..STACK_COUNT {
-        stacks.push(Vec::new());
-    }
+    let mut stacks = vec![Vec::new(); STACK_COUNT];
     for line in lines.by_ref().take(MAX_STACK) {
         let len = line.len();
         for (i, stack) in stacks.iter_mut().enumerate() {
