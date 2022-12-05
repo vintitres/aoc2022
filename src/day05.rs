a08a98af006e1b2178bb1981649bd7a4b38a1030
vintitres <jaqb@sekta.pl>
2022-12-05 06:36:01 +0000

    5part1mess

--- src/day05.rs
+++ src/day05.rs
@@ -1,13 +1,53 @@
-fn read(input: &str) -> impl Iterator<Item = &str> + '_ {
-    input.lines()
+use itertools::Itertools;
+
+const MAX_STACK: usize = 8;
+const STACK_COUNT: usize = 9;
+// const MAX_STACK: usize = 3;
+// const STACK_COUNT: usize = 3;
+fn read(input: &str) -> (Vec<Vec<char>>, Vec<(usize, usize, usize)>) {
+    let mut lines = input.lines();
+    let mut stacks = Vec::new();
+    for _ in 0..STACK_COUNT {
+        stacks.push(Vec::new());
+    }
+    for line in lines.by_ref().take(MAX_STACK) {
+        let len = line.len();
+        for i in 0..STACK_COUNT {
+            if 4 * i + 1 < len {
+                let c = line.chars().nth(4 * i + 1).unwrap();
+                if c != ' ' {
+                    stacks[i].push(c);
+                }
+            }
+        }
+    }
+    for mut stack in &mut stacks {
+        stack.reverse()
+    }
+    let moves = lines.skip(2).map(|l| {
+        l.split(' ').map(|w| w.parse()).flatten().collect_tuple().unwrap()
+    }).collect();
+    (stacks, moves)
 }
 
-pub fn part1(input: &str) -> usize {
-    read(input).count()
+pub fn part1(input: &str) -> String {
+    let (mut stacks, moves) = read(input);
+    for (count, from, to) in moves {
+        let to = to -1;
+        let from = from - 1;
+        let new_len = stacks[from].len() - count;
+        let moved = stacks[from][new_len..].iter().rev().cloned().collect_vec();
+        stacks.get_mut(to).unwrap().extend(moved);
+        stacks[from].resize(new_len, '!');
+        println!("{:?}", stacks);
+    }
+    // format!("{:?}", stacks)
+    stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
 
 pub fn part2(input: &str) -> usize {
-    read(input).count()
+    // read(input).count()
+    0
 }
 
 #[cfg(test)]
@@ -20,11 +60,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 466);
+        assert_eq!(part1(input()), "");
     }
 
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(input()), 865);
-    }
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(part2(input()), 865);
+    // }
 }
