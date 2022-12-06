4cb745acf48f9e0be44f7f64e60887091c53160d
vintitres <jaqb@sekta.pl>
2022-12-06 06:27:37 +0000

    7

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-#[cfg(alldays)]
+#[cfg(aoc_alldays)]
 pub mod day01;
-#[cfg(alldays)]
+#[cfg(aoc_alldays)]
 pub mod day02;
-#[cfg(alldays)]
+#[cfg(aoc_alldays)]
 pub mod day03;
-#[cfg(alldays)]
+#[cfg(aoc_alldays)]
 pub mod day04;
-#[cfg(alldays)]
+#[cfg(aoc_alldays)]
 pub mod day05;
+#[cfg(aoc_alldays)]
 pub mod day06;
+pub mod day07;
 
 // use aoc_runner_derive::aoc_lib;
 // aoc_lib! { year = 2022 }
