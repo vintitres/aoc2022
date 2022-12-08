e6a5162bac41036efb893338a3f3a8a700a8a26f
vintitres <jaqb@sekta.pl>
2022-12-08 06:14:07 +0000

    8

--- src/day08.rs
+++ src/day08.rs
@@ -55,8 +55,47 @@ pub fn part1(input: &str) -> usize {
 
 }
 
-pub fn part2(_input: &str) -> u32 {
-    1 + 2
+pub fn part2(input: &str) -> u32 {
+    let forest = input.lines().map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as i32).collect_vec()).collect_vec();
+    let mut max = 0;
+    for i in 0..forest.len() {
+        for j in 0..forest[0].len() {
+            let mut seen = (0,0,0,0);
+            for ii in i+1..forest.len() {
+               if forest[ii][j] < forest[i][j] {
+                seen.0 +=1;
+               } else {
+                seen.0 +=1;
+                break;
+               }
+            }
+            for ii in (0..i.checked_div(1).unwrap_or(0)).rev() {
+               if forest[ii][j] < forest[i][j] {
+                seen.1 +=1;
+               } else {
+                seen.1 +=1;
+                break;
+               }
+            }
+            for jj in j+1..forest[0].len() {
+               if forest[i][jj] < forest[i][j] {
+                seen.2 +=1;
+               } else {
+                seen.2 +=1;
+                break;
+               }
+            }
+            for jj in (0..j.checked_div(1).unwrap_or(0)).rev() {
+                seen.3 +=1;
+               if forest[i][jj] >= forest[i][j] {
+                break;
+               }
+            }
+            println!("{} {}: {:?}", i, j, seen);
+            max = core::cmp::max(max, seen.0*seen.1*seen.2*seen.3);
+        }
+    }
+    max
 }
 
 #[cfg(test)]
@@ -69,11 +108,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 1);
+        assert_eq!(part1(input()), 1647);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 3);
+        assert_eq!(part2(input()), 392080);
     }
 }
