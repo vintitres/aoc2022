3e789efca73bb95958a52b4749da00bd5cb09a97
vintitres <jaqb@sekta.pl>
2022-12-09 06:02:28 +0000

    9

--- src/day09.rs
+++ src/day09.rs
@@ -3,21 +3,49 @@ use std::collections::BTreeSet;
 use itertools::Itertools;
 
 type Mov = (i32, i32);
+type Pos = (i32, i32);
 
 pub fn read(input: &str) -> Vec<(Mov, usize)> {
-    input.lines().map(|line| {
-        println!("{}", line);
-        let (dir, cnt) = line.split(' ').collect_tuple().unwrap();
-        (match dir.chars().next().unwrap() {
-            'D' => (0, -1),
-            'U' => (0, 1),
-            'R' => (1, 0),
-            'L' => (-1, 0),
-            _ => unimplemented!(),
-        }, cnt.parse::<usize>().unwrap())
-    }).collect_vec()
+    input
+        .lines()
+        .map(|line| {
+            println!("{}", line);
+            let (dir, cnt) = line.split(' ').collect_tuple().unwrap();
+            (
+                match dir.chars().next().unwrap() {
+                    'D' => (0, -1),
+                    'U' => (0, 1),
+                    'R' => (1, 0),
+                    'L' => (-1, 0),
+                    _ => unimplemented!(),
+                },
+                cnt.parse::<usize>().unwrap(),
+            )
+        })
+        .collect_vec()
 }
 
+fn follow(back: &mut Pos, front: &Pos) {
+    let d0 = front.0 - back.0;
+    let d1 = front.1 - back.1;
+    match (d0.abs(), d1.abs()) {
+        (0, 0) => {}
+        (0, 1) => {}
+        (1, 0) => {}
+        (1, 1) => {}
+        (2, 0) => back.0 += d0.signum(),
+        (0, 2) => back.1 += d1.signum(),
+        (1, 2) => {
+            back.0 += d0.signum();
+            back.1 += d1.signum();
+        }
+        (2, 1) => {
+            back.0 += d0.signum();
+            back.1 += d1.signum();
+        }
+        _ => unimplemented!(),
+    }
+}
 
 pub fn part1(input: &str) -> usize {
     let moves = read(input);
@@ -29,25 +57,7 @@ pub fn part1(input: &str) -> usize {
         for _ in 0..cnt {
             head.0 += mv.0;
             head.1 += mv.1;
-            let d0 = head.0 - tail.0;
-            let d1 = head.1 - tail.1;
-            match (d0.abs(), d1.abs()) {
-                (0, 0) => {},
-                (0, 1) => {},
-                (1, 0) => {},
-                (1, 1) => {},
-                (2, 0) => tail.0 += d0.signum(),
-                (0, 2) => tail.1 += d1.signum(),
-                (1, 2) => {
-                    tail.0 += d0.signum();
-                    tail.1 += d1.signum();
-                }
-                (2, 1) => {
-    tail.0 += d0.signum();
-                    tail.1 += d1.signum();
-                }
-                _ => unimplemented!(),
-            }
+            follow(&mut tail, &head);
             visited.insert(tail);
         }
     }
