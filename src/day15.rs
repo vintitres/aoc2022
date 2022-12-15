c0d0ce6e7de59731912b167175dc6463c301a3c7
vintitres <jaqb@sekta.pl>
2022-12-15 23:51:16 +0000

    15b

--- src/day15.rs
+++ src/day15.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeSet;
 
 use itertools::Itertools;
 
-type Pos = (i32, i32);
+type Pos = (i64, i64);
 
 #[derive(Debug)]
 struct Sensor {
@@ -40,10 +40,10 @@ impl Sensor {
             closest_beacon,
         }
     }
-    fn closest_beacon_dist(&self) -> i32 {
+    fn closest_beacon_dist(&self) -> i64 {
         dist(self.pos, self.closest_beacon)
     }
-    fn blocked_at_y(&self, y: i32) -> impl Iterator<Item = i32> {
+    fn blocked_at_y(&self, y: i64) -> impl Iterator<Item = i64> {
         let bd = self.closest_beacon_dist();
         let yd = (self.pos.1 - y).abs();
         let fd = bd - yd;
@@ -51,9 +51,22 @@ impl Sensor {
         let r = self.pos.0 + fd;
         l..r
     }
+
+    fn border(&self) -> impl Iterator<Item = Pos> {
+        let bd = self.closest_beacon_dist() + 1;
+        let lt = (self.pos.0-bd..self.pos.0).zip((self.pos.1-bd..self.pos.1).rev());
+        let rt = (self.pos.0..self.pos.0+bd).zip(self.pos.1-bd..self.pos.1);
+        let lb = (self.pos.0-bd..self.pos.0).zip(self.pos.1..self.pos.1+bd);
+        let rb = (self.pos.0..self.pos.0+bd).zip((self.pos.1..self.pos.1+bd).rev());
+
+        lt.chain(rt).chain(lb).chain(rb).filter(|(x,y)| *x >= 0 && *x <= 4000000 && *y >= 0 && *y <= 4000000)
+    }
+    fn in_range(&self, p: Pos) -> bool {
+        dist(self.pos, p) <= self.closest_beacon_dist()
+    }
 }
 
-fn dist(p1: Pos, p2: Pos) -> i32 {
+fn dist(p1: Pos, p2: Pos) -> i64 {
     (p1.0 - p2.0).abs() + (p1.1- p2.1).abs()
 }
 
@@ -65,8 +78,21 @@ pub fn part1(input: &str) -> usize {
     positions.len()
 }
 
-pub fn part2(input: &str) -> usize {
-    input.len()
+pub fn part2(input: &str) -> i64 {
+    let sensors = input.lines().map(Sensor::read).collect_vec();
+    for s in &sensors {
+        let border = s.border();
+        for p in border {
+            match sensors.iter().find(|s| s.in_range(p)) {
+                None => {
+                    println!("{:?}", p);
+                    return p.1 + p.0 * 4000000;
+                },
+                Some(_) => continue,
+            }
+        }
+    };
+    unimplemented!();
 }
 
 #[cfg(test)]
