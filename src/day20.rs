550e3cc0a2e9dc9a6f6c17778c969a20040659cb
Jakub Woyke <jaqb@sekta.pl>
2022-12-21 16:14:52 +0000

    20todo

--- src/day20.rs
+++ src/day20.rs
@@ -1,5 +1,24 @@
+struct Elem {
+    val: i32,
+    moved: bool,
+}
+
+impl Elem {
+    fn read(sval: &str) -> Elem {
+        Elem {
+            val: sval.parse().unwrap(),
+            moved: false,
+        }
+    }
+}
+
 pub fn part1(input: &str) -> usize {
-    input.len()
+    let mut l = LinkedList::from_iter(input.split(' ').map(Elem::read));
+    let ll = l.len();
+    for _ in 0..ll {
+        // TODO move first not moved
+    }
+    vec![1000, 2000, 3000, 4000].iter().map(|i| l.nth(1000 % ll).unwrap()).product()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -14,13 +33,11 @@ mod tests {
         include_str!("../input/2022/day20.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
         assert_eq!(part1(input()), 15120);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
         assert_eq!(part2(input()), 1234);
