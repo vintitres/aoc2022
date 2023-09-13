37baebf213822c00667ea69c94ae7b99a31be607
vintitres <jaqb@sekta.pl>
2023-09-13 13:34:19 +0000

    18a

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,15 @@
 use itertools::Itertools;
 use std::collections::HashSet;
+use std::collections::VecDeque;
+
+const SIDES: [(i32, i32, i32); 6] = [
+    (0, 0, 1),
+    (0, 0, -1),
+    (0, 1, 0),
+    (0, -1, 0),
+    (1, 0, 0),
+    (-1, 0, 0),
+];
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Cube {
@@ -18,21 +28,13 @@ impl Cube {
     }
 }
 
-pub fn part1(input: &str) -> i64 {
+pub fn part1(input: &str) -> i32 {
     let cubes: HashSet<Cube> = input.lines().map(Cube::read).collect();
-    let cubes2 = cubes.clone();
     let mut surface = 0;
-    for cube in cubes {
-        for side in [
-            (0, 0, 1),
-            (0, 0, -1),
-            (0, 1, 0),
-            (0, -1, 0),
-            (1, 0, 0),
-            (-1, 0, 0),
-        ] {
+    for cube in &cubes {
+        for side in SIDES {
             let (sx, sy, sz) = side;
-            if !cubes2.contains(&Cube {
+            if !cubes.contains(&Cube {
                 x: cube.x + sx,
                 y: cube.y + sy,
                 z: cube.z + sz,
@@ -44,8 +46,33 @@ pub fn part1(input: &str) -> i64 {
     surface
 }
 
-pub fn part2(input: &str) -> usize {
-    input.len()
+pub fn part2(input: &str) -> i32 {
+    let cubes: HashSet<Cube> = input.lines().map(Cube::read).collect();
+    let max_x = cubes.iter().map(|c| c.x).max().unwrap() + 5;
+    let max_y = cubes.iter().map(|c| c.y).max().unwrap() + 5;
+    let max_z = cubes.iter().map(|c| c.z).max().unwrap() + 5;
+    let mut q = VecDeque::new();
+    let mut seen = HashSet::new();
+    let start = (0, 0, 0);
+    q.push_back(start);
+    seen.insert(start);
+    let mut surface = 0;
+    while !q.is_empty() {
+        let (px, py, pz) = q.pop_front().unwrap();
+        for side in SIDES {
+            let (sx, sy, sz) = side;
+            let (x, y, z) = (px + sx, py + sy, pz + sz);
+            if (x >= 0 && x <= max_x && y >= 0 && y <= max_y && z >= 0 && z <= max_z) {
+                if cubes.contains(&Cube { x, y, z }) {
+                    surface += 1;
+                } else if (!seen.contains(&(x, y, z))) {
+                    q.push_back((x, y, z));
+                    seen.insert((x, y, z));
+                }
+            }
+        }
+    }
+    surface
 }
 
 #[cfg(test)]
@@ -53,7 +80,7 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        include_str!("../input/2022/day18.txt")
+        include_str!("../input/2022/day18e.txt")
     }
 
     #[test]
