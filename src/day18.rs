21778312784cdf82a500b1081edbe3c50ae3f9e2
vintitres <jaqb@sekta.pl>
2023-09-13 13:02:27 +0000

    18a

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,47 @@
-pub fn part1(input: &str) -> usize {
-    input.len()
+use itertools::Itertools;
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct Cube {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+impl Cube {
+    fn read(line: &str) -> Self {
+        let (x, y, z) = line.splitn(3, ',').collect_tuple().unwrap();
+        Cube {
+            x: x.parse().unwrap(),
+            y: y.parse().unwrap(),
+            z: z.parse().unwrap(),
+        }
+    }
+}
+
+pub fn part1(input: &str) -> i64 {
+    let cubes: HashSet<Cube> = input.lines().map(Cube::read).collect();
+    let cubes2 = cubes.clone();
+    let mut surface = 0;
+    for cube in cubes {
+        for side in vec![
+            (0, 0, 1),
+            (0, 0, -1),
+            (0, 1, 0),
+            (0, -1, 0),
+            (1, 0, 0),
+            (-1, 0, 0),
+        ] {
+            let (sx, sy, sz) = side;
+            if !cubes2.contains(&Cube {
+                x: cube.x + sx,
+                y: cube.y + sy,
+                z: cube.z + sz,
+            }) {
+                surface += 1;
+            }
+        }
+    }
+    surface
 }
 
 pub fn part2(input: &str) -> usize {
@@ -16,7 +58,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 15120);
+        assert_eq!(part1(input()), 3326);
     }
 
     #[test]
