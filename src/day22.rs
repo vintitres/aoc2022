e671f8813c8cc4cdc7ae5dafc43471a949d6fdf4
vintitres <jaqb@sekta.pl>
2023-09-14 16:31:10 +0000

    22onlyexamplecubepattern

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,7 @@
 use itertools::Itertools;
 
+const L: usize = 50;
+
 #[derive(Debug)]
 enum Wise {
     Counterclockwise,
@@ -155,33 +157,93 @@ fn step_fn_cube(
     pos: (usize, usize),
     face: Facing,
 ) -> Option<((usize, usize), Facing)> {
-    let mut pos = pos;
-    loop {
-        let new_pos = match face {
-            Facing::Down => (if pos.0 + 1 == map.len() { 0 } else { pos.0 + 1 }, pos.1),
-            Facing::Up => (if pos.0 == 0 { map.len() - 1 } else { pos.0 - 1 }, pos.1),
-            Facing::Left => (
-                pos.0,
-                if pos.1 == 0 {
-                    map[pos.0].len() - 1
-                } else {
-                    pos.1 - 1
-                },
-            ),
-            Facing::Right => (
-                pos.0,
-                if pos.1 + 1 == map[pos.0].len() {
-                    0
-                } else {
-                    pos.1 + 1
-                },
-            ),
-        };
-        match map[new_pos.0].get(new_pos.1) {
-            Some('#') => return None,
-            Some('.') => return Some((new_pos, face)),
-            _ => pos = new_pos,
+    let (new_pos, new_face) = match face {
+        /*
+              12-----|
+              |     1111
+              |     1111
+              |  13-1111------16
+              |  |  1111       |
+            222233334444       |
+            222233334444-46    |
+        |---222233334444  |    |
+        |   222233334444  |    |
+        |     |  |  55556666   |
+        |     |  35-55556666---|
+        |     |     55556666
+        |     |     55556666
+        |     25-----|    |
+        26----------------|
+
+
+        */
+        Facing::Down => {
+            if pos.0 == 2 * L - 1 && pos.1 < L {
+                // 25
+                ((3 * L - 1, 2 * L + L - 1 - pos.1), Facing::Up)
+            } else if pos.0 == 2 * L - 1 && pos.1 >= L && pos.1 < 2 * L {
+                // 35
+                ((2 * L + 2 * L - 1 - pos.1, 2 * L), Facing::Right)
+            } else if pos.0 == 3 * L && pos.1 >= 2 * L && pos.1 < 3 * L {
+                // 52
+                ((2 * L, 3 * L - 1 - pos.1), Facing::Up)
+            } else if pos.0 == 3 * L && pos.1 >= 3 * L {
+                // 62
+                ((L + 4 * L - 1 - pos.1, 0), Facing::Right)
+            } else {
+                ((pos.0 + 1, pos.1), face)
+            }
         }
+        Facing::Up => {
+            if pos.1 < L && pos.0 == L - 1 {
+                // 21
+                ((0, 2 * L + L - 1 - pos.1), Facing::Down)
+            } else if pos.1 >= L && pos.1 < 2 * L && pos.0 == L - 1 {
+                // 31
+                ((pos.1 - L, 2 * L), Facing::Right)
+            } else if pos.0 == 0 && pos.1 >= 2 * L && pos.1 < 3 * L {
+                // 12
+                ((L, 3 * L - 1 + pos.1), Facing::Down)
+            } else if pos.0 == 2 * L && pos.1 >= 3 * L {
+                // 64
+                ((L + 4 * L - 1 - pos.1, 3 * L - 1), Facing::Left)
+            } else {
+                ((pos.0 - 1, pos.1), face)
+            }
+        }
+        Facing::Left => {
+            if pos.0 < L && pos.1 == 2 * L {
+                // 13
+                ((L, L + pos.0), Facing::Down)
+            } else if pos.0 >= L && pos.0 < 2 * L && pos.1 == 0 {
+                // 26
+                ((3 * L - 1, 3 * L + 2 * L - 1 - pos.0), Facing::Up)
+            } else if pos.0 >= 2 * L && pos.1 == 2 * L {
+                // 53
+                ((3 * L - 1, L + 3 * L - 1 - pos.0), Facing::Up)
+            } else {
+                ((pos.0, pos.1 - 1), face)
+            }
+        }
+        Facing::Right => {
+            if pos.0 < L && pos.1 == 3 * L - 1 {
+                // 16
+                ((2 * L + L - 1 - pos.0, 4 * L - 1), Facing::Left)
+            } else if pos.0 >= L && pos.0 < 2 * L && pos.1 == 3 * L - 1 {
+                // 46
+                ((2 * L, 3 * L + 2 * L - 1 - pos.0), Facing::Down)
+            } else if pos.0 >= 2 * L && pos.1 == 4 * L - 1 {
+                // 61
+                ((3 * L - 1 - pos.0, 3 * L - 1), Facing::Left)
+            } else {
+                ((pos.0, pos.1 + 1), face)
+            }
+        }
+    };
+    match map[new_pos.0].get(new_pos.1) {
+        Some('#') => None,
+        Some('.') => Some((new_pos, new_face)),
+        _ => panic!("out of bounds: {:?}", pos),
     }
 }
 pub fn part2(input: &str) -> usize {
