e5f0074ce40e841f5ba69d0fb334e1d96c2a883e
vintitres <jaqb@sekta.pl>
2023-09-15 16:53:02 +0000

    fmt

--- src/day25.rs
+++ src/day25.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 
 const SNAFU_BASE: i8 = 5;
 struct SNAFU {
-    digits: Vec<i8>
+    digits: Vec<i8>,
 }
 
 impl SNAFU {
@@ -17,8 +17,13 @@ impl SNAFU {
         }
     }
     fn parse(input: &str) -> Self {
-        Self { digits:
-        input.chars().rev().map(|c| Self::parse_digit(c)).collect_vec() }
+        Self {
+            digits: input
+                .chars()
+                .rev()
+                .map(|c| Self::parse_digit(c))
+                .collect_vec(),
+        }
     }
     fn to_dec(&self) -> i64 {
         let mut p: i64 = 1;
@@ -38,7 +43,7 @@ impl SNAFU {
                 *d += 1;
                 added = true;
                 break;
-            }       
+            }
         }
         if !added {
             self.digits.push(2);
@@ -55,9 +60,8 @@ impl SNAFU {
     fn push(&mut self, v: i8) {
         match v {
             1 | 2 => self.digits.push(v),
-            _ => panic!("bad push digit value: {}", v)
+            _ => panic!("bad push digit value: {}", v),
         }
-
     }
     fn from_dec(n: i64) -> Self {
         if n == 0 {
@@ -86,13 +90,12 @@ impl SNAFU {
         let mut ss = Self::from_dec(n - s);
         ss.push(1);
         return ss;
-        
 
         // 1bits: 2 * 5^0 =   2    2
         // 2bits: 2 * 5^1 =  10   12
         // 3bits: 2 * 5^2 =  50   62
         // 4bits: 2 * 5^3 = 250  312
-        // 5bits: 2 * 5^4 =1250 1562 
+        // 5bits: 2 * 5^4 =1250 1562
 
         // 10    20
         // 11    21
@@ -115,24 +118,25 @@ impl SNAFU {
         // 28   11=
         // 29   11-
         // 30   110
-        
     }
     fn to_str(&self) -> String {
-        self.digits.iter().rev().map (|d|
-            match d {
+        self.digits
+            .iter()
+            .rev()
+            .map(|d| match d {
                 -2 => '=',
                 -1 => '-',
                 0 => '0',
                 1 => '1',
                 2 => '2',
-                _ => panic!("unexpected SNAFTU digit val: {}", d)
-            }
-        ).collect()
+                _ => panic!("unexpected SNAFTU digit val: {}", d),
+            })
+            .collect()
     }
 }
 
 pub fn part1(input: &str) -> String {
-    let fuelsum : i64 = input.lines().map(|l| SNAFU::parse(l).to_dec()).sum();
+    let fuelsum: i64 = input.lines().map(|l| SNAFU::parse(l).to_dec()).sum();
     // fuelsum.to_string()
     SNAFU::from_dec(fuelsum).to_str()
 }
